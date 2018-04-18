ec163be1386ca1e692fb2446dac33a4a131fc090
amusamhe <abdalsamad.y.m@gmail.com>
2018-04-18 13:30:22 +0300

    insertBooks

--- src/database/queries/insertbooks.js
+++ src/database/queries/insertbooks.js
@@ -1,7 +1,7 @@
 const db_connection = require('../db_connection');
-let sql = { text:'INSERT INTO books (book_name, year, author) VALUES ($1, $2, $3)', values: [book_name, year, author]};
 
-const insertBooks = (cb) => {
+const insertBooks = (book_name,year,author, cb) => {
+  let sql = { text:'INSERT INTO books (book_name, year, author) VALUES ($1, $2, $3)', values: [book_name, year, author]};
   db_connection.query(sql,(err, res) => {
 
       if (err) {
@@ -16,6 +16,3 @@ const insertBooks = (cb) => {
 module.exports = {
   insertBooks
 };
-
-
-' Hard Times ', 1850 ,' charles Dickens'
