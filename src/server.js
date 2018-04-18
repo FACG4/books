a38e1b61a9f7a4487c7598c8390287f2f571c2d8
haneenshahwan4 <haneenshahwan@hotmail.com>
2018-04-18 16:58:37 +0300

    heroku

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const router = require('./router');
 
 const port = process.env.PORT || 3000;
-const host = process.env.HOSTNAME || 'localhost';
+const host = process.env.HOST || 'localhost';
 
 const server = http.createServer(router);
 server.listen(port, host, () => {
